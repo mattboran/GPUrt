ae5a7dffa8c83e1e907b4ca26ae7286f0388f2d5
mattboran <mattboran@gmail.com>
2016-06-28 15:22:54 -0400

    Got sphere list out of __constant__ memory and into device global memory (almost, anyway)

--- rayTracerNoGL.cu
+++ rayTracerNoGL.cu
@@ -31,7 +31,7 @@ enum Geom_t { SPH, TRI, BOX };
 
 //Sphere - primitive object defined by radius and center.
 //All primitives also have emmission (light, a vector) and color (another vector)
-struct Sphere{
+struct __declspec(align(64)) Sphere{
 	float rad;
 	float3 cent, emit, col; //center, emission, color
 	Refl_t refl; //material type
@@ -70,8 +70,6 @@ struct Triangle{
 	__device__ Triangle(float3 x, float3 y, float3 z, float3 e, float3 c, Refl_t r) :
 		v1(x), v2(y), v3(z), emit(e), col(c), refl(r), geomtype(TRI){}
 
-	__global__ Triangle(float3 x, float3 y, float3 z, float3 e, float3 c, Refl_t r) :
-		v1(x), v2(y), v3(z), emit(e), col(c), refl(r), geomtype(TRI){}
 
 	//Moller-Trumbore ray-triangle intersection.
 	//consider storing triangles as 1 vertex and 2 edges for faster compute
@@ -110,9 +108,13 @@ struct Triangle{
 		return 0.f;
 	}
 };
+
+Sphere *dev_sphere_ptr;
+float *dev_tri_ptr;
+
 //These numbers come directly from smallPT
 //had to scale everything down by a factor of 10 to reduce artifacts.
-__constant__ Sphere spheres[] = {
+Sphere spheres[] = {
 	{ 1e4f, { 1e4f + .10f, 4.08f, 8.16f }, { 0.0f, 0.0f, 0.0f }, { 0.75f, 0.25f, 0.25f }, DIFF }, //Left 
 	{ 1e4f, { -1e4f + 9.90f, 4.08f, 8.16f }, { 0.0f, 0.0f, 0.0f }, { .25f, .25f, .75f }, DIFF }, //Rght 
 	{ 1e4f, { 5.00f, 4.08f, 1e4f }, { 0.0f, 0.0f, 0.0f }, { .75f, .75f, .75f }, DIFF }, //Back 
@@ -124,14 +126,25 @@ __constant__ Sphere spheres[] = {
 	{ 60.0f, { 5.00f, 68.16f - .05f, 8.16f }, { 2.0f, 1.8f, 1.6f }, { 0.0f, 0.0f, 0.0f }, DIFF }  // Light
 };
 
+void loadSpheresToMemory(Sphere *sph_list, int numberofspheres){
+
+	size_t numspheres = numberofspheres * sizeof(Sphere);
+	cudaMalloc((void **)&dev_sphere_ptr, numspheres);
+	cudaMemcpy(dev_sphere_ptr, &sph_list[0], numspheres, cudaMemcpyHostToDevice);
+	printf("%d numspheres\n", numspheres);
+
+}
+
 //World description: 9 spheres that form a modified Cornell box. this can be kept in const GPU memory (for now)
-__device__ inline bool intersectScene(const Ray &r, float &t, int &id){
-	float n = sizeof(spheres) / sizeof(Sphere); //get number of spheres by memory size
+__device__ inline bool intersectScene(const Ray &r, float &t, int &id, Sphere *sphere_list){
+	//float n = sizeof(spheres) / sizeof(Sphere); //get number of spheres by memory size
+	float numspheres = sizeof(sphere_list) / sizeof(Sphere);
+	numspheres = 9;
 	float tprime;
 	float inf = 1e15f;
 	t = inf; //initialize t to infinite distance
-	for (int i = int(n); i >= 0; i--){//cycle through all spheres, until i<0
-		if ((tprime = spheres[i].intersectSphere(r)) && tprime < t){//new intersection is closer than previous closest
+	for (int i = int(numspheres); i >= 0; i--){//cycle through all spheres, until i<0
+		if ((tprime = sphere_list[i].intersectSphere(r)) && tprime < t){//new intersection is closer than previous closest
 			t = tprime;
 			id = i; //store hit sphere by ID (array index)
 		}
@@ -169,23 +182,23 @@ __device__ inline float getMax(float3 f){
 //by reflectence function of material, weighted by cosine incidence angle (Lambert's cosine law)
 //Inputs: ray to calculate radiance along, and seeds for random num generation.
 //Output: color at point.
-__device__ float3 radiance(Ray &r, curandState *randstate){
+__device__ float3 radiance(Ray &r, curandState *randstate, Sphere *sphere_list){
 	float3 accumulated = make_float3(0.f, 0.f, 0.f); //accumulated ray color for each iteration of loop
 	float3 mask = make_float3(1.f, 1.f, 1.f);
 
 	//ray bounce loop, will use Russian Roulette later
-	for (int bounces = 0; bounces < 8; bounces++){ //this iterative loop replaces recursive path tracing method; max depth is 4 bounces (no RR)
+	for (int bounces = 0; bounces < 20; bounces++){ //this iterative loop replaces recursive path tracing method; max depth is 4 bounces (no RR)
 
 
 		float t; //distance to hitt
 		int id = 0; //index of hit 
 		float3 d; //next ray direction
 
-		if (!intersectScene(r, t, id))
+		if (!intersectScene(r, t, id, sphere_list))
 			return make_float3(0.f, 0.f, 0.f); //return background color of 0 if no hit
 
 		//if the loop gets to here, we have hit. compute hit point and surface normal
-		const Sphere &hitobj = spheres[id];
+		const Sphere &hitobj = sphere_list[id];
 		float3 hitpt = r.origin + r.dir*t;
 		float3 hitnorm = normalize(hitpt - hitobj.cent); //surface normal
 		//reverse normal if going through object - used to determine where we are for refraction
@@ -283,7 +296,7 @@ __device__ float3 radiance(Ray &r, curandState *randstate){
 
 //this is the main rendering kernel that can be called from the CPU, runs in parallel on CUDA threads.
 //each pixel runs in parallel
-__global__ void render_kernel(float3 *out, uint hashedSampleNumber){
+__global__ void render_kernel(float3 *out, uint hashedSampleNumber, Sphere *sphere_list){
 	//assign thread to every pixel
 	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
 	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
@@ -307,7 +320,7 @@ __global__ void render_kernel(float3 *out, uint hashedSampleNumber){
 		float3 dir = cam.dir + ofs_x * ((.25f + x) / XRES - 0.5f + curand_uniform(&randstate) / XRES) + ofs_y * ((.25f + y) / YRES - 0.5f + curand_uniform(&randstate) / YRES);
 		//create incoming ray, add incoming radiance to final_col; push ray to start inside sphere that forms wall where we view from
 		//that way, the scene does not distort at the edges
-		col = col + radiance(Ray(cam.origin + dir * 4.f, normalize(dir)), &randstate) * (1.f / SAMPLES);
+		col = col + radiance(Ray(cam.origin + dir * 4.f, normalize(dir)), &randstate, sphere_list) * (1.f / SAMPLES);
 	}
 	//write rgb value of pixel to image buffer on GPU, clamped on [0.0f, 1.0f]
 	out[i] = make_float3(clamp(col.x, 0.f, 1.f), clamp(col.y, 0.f, 1.f), clamp(col.z, 0.f, 1.f));
@@ -330,6 +343,9 @@ int main()
 	//allocate memory in GPU VRAM
 	cudaMalloc(&out_dvc, XRES * YRES * sizeof(float3));
 
+	//load spheres to memory
+	loadSpheresToMemory(spheres, 9);
+
 	//dim3 block and grid are CUDA specific for scheduling CUDA threads of stream processors
 	dim3 block(16, 16, 1); //256 threads per block
 	dim3 grid(XRES / block.x, YRES / block.y, 1);//number of blocks
@@ -344,7 +360,7 @@ int main()
 	//start timer
 	cudaEventRecord(start);
 	//schedule GPU threads and launch kernel from host CPU
-	render_kernel << < grid, block >> > (out_dvc, hash(124));
+	render_kernel << < grid, block >> > (out_dvc, hash(124), dev_sphere_ptr);
 
 	//copy result back to host and free cuda memory
 	cudaMemcpy(out_host, out_dvc, XRES * YRES * sizeof(float3), cudaMemcpyDeviceToHost);
